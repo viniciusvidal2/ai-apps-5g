ff4b68e5351ce3bb14d7050e7e3080e13303aec6
viniciusvidal2 <viniciusvidal2@gmail.com>
2025-02-21 12:19:22 -0300

    removing unused script. Organizing workflows in a separate folder

--- workflows/audio_to_report.py
+++ workflows/audio_to_report.py
@@ -1,12 +1,19 @@
 import os
 import time
 import argparse
-from ai_apis.summarize_text import runBartSummarizer
+import sys
+
+# Add the parent directory folder to find our modules
+root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(root_path)
+
 from ai_apis.audio_to_text import runWhisper
+from ai_apis.report_generation import generateReportWithModel
+from ai_apis.pull_model_ollama import pullModel
 
 
 def main(audio_path: str) -> None:
-    """Generates a summary from an audio file.
+    """Generates a report from an audio file.
 
     Args:
         audio_path (str): input audio file path
@@ -14,27 +21,33 @@ def main(audio_path: str) -> None:
     # Measuring time
     start_time = time.time()
 
-    # Define model ids
-    summarizer_model_id = "facebook/bart-large-cnn"
+    # Getting text from the audio
     whisper_model_id = "openai/whisper-large-v3-turbo"
-
-    # Calling the models in sequence
     text_prompt = runWhisper(model_id=whisper_model_id, audio_path=audio_path)
-    summary = runBartSummarizer(
-        text_prompt=text_prompt, model_id=summarizer_model_id, min_length_pct=0.1, max_length_pct=0.5)
-    
+
+    # Pull the proper ollama model
+    ollama_model_id = "llama3.2"
+    model_downloaded = pullModel(model_id=ollama_model_id)
+    if not model_downloaded:
+        print("Error downloading the model")
+        return
+
+    # Generate the report
+    report = generateReportWithModel(
+        model_id=ollama_model_id, message=text_prompt)
+
     time_elapsed = time.time() - start_time
 
     print("This is the input text:\n")
     print(text_prompt)
-    print("\n\nThis is the output summary:")
-    print(summary)
+    print("\n\nThis is the output report:")
+    print(report)
     print(f"\nTime elapsed: {time_elapsed} seconds")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Process an audio file and generate a summary.")
+        description="Process an audio file and generate a report.")
     parser.add_argument("--audio_file", type=str, help="Path to the audio file",
                         default=os.path.join(os.getenv("HOME"), "Downloads/secao_3.mpeg"))
     args = parser.parse_args()
--- workflows/audio_to_summary.py
+++ workflows/audio_to_summary.py
@@ -1,13 +1,18 @@
 import os
 import time
 import argparse
-from ai_apis.pull_model_ollama import pullModel
-from ai_apis.report_generation import generateReportWithModel
+import sys
+
+# Add the parent directory folder to find our modules
+root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(root_path)
+
+from ai_apis.summarize_text import runBartSummarizer
 from ai_apis.audio_to_text import runWhisper
 
 
 def main(audio_path: str) -> None:
-    """Generates a report from an audio file.
+    """Generates a summary from an audio file.
 
     Args:
         audio_path (str): input audio file path
@@ -15,33 +20,27 @@ def main(audio_path: str) -> None:
     # Measuring time
     start_time = time.time()
 
-    # Getting text from the audio
+    # Define model ids
+    summarizer_model_id = "facebook/bart-large-cnn"
     whisper_model_id = "openai/whisper-large-v3-turbo"
+
+    # Calling the models in sequence
     text_prompt = runWhisper(model_id=whisper_model_id, audio_path=audio_path)
-
-    # Pull the proper ollama model
-    ollama_model_id = "llama3.2"
-    model_downloaded = pullModel(model_id=ollama_model_id)
-    if not model_downloaded:
-        print("Error downloading the model")
-        return
-
-    # Generate the report
-    report = generateReportWithModel(
-        model_id=ollama_model_id, message=text_prompt)
-
+    summary = runBartSummarizer(
+        text_prompt=text_prompt, model_id=summarizer_model_id, min_length_pct=0.1, max_length_pct=0.5)
+    
     time_elapsed = time.time() - start_time
 
     print("This is the input text:\n")
     print(text_prompt)
-    print("\n\nThis is the output report:")
-    print(report)
+    print("\n\nThis is the output summary:")
+    print(summary)
     print(f"\nTime elapsed: {time_elapsed} seconds")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Process an audio file and generate a report.")
+        description="Process an audio file and generate a summary.")
     parser.add_argument("--audio_file", type=str, help="Path to the audio file",
                         default=os.path.join(os.getenv("HOME"), "Downloads/secao_3.mpeg"))
     args = parser.parse_args()

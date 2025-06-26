088775eecb6bb366fae3d0f919f17f9de8a718a1
viniciusvidal2 <viniciusvidal2@gmail.com>
2025-06-26 16:33:45 -0300

    refactor to agents and workflows. improving app and chatbot helper to use dockers

--- orchestrator/app.py
+++ orchestrator/app.py
@@ -1,25 +1,87 @@
+import paho.mqtt.client as mqtt
 import streamlit as st
 import os
 import sys
+import argparse
+import yaml
 from helper_chatbot import chatbot_ui
 
 # Add the parent directory folder to find our modules
 root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(root_path)
 
-st.set_page_config(page_title="Assistente Inteligente SAE", layout="wide")
-st.title("Assistente Inteligente SAE")
 
-# Create the tabs
-tab_titles = ["Chatbot", "Redes Neurais por planilha", "Análise de PDFs"]
-tabs = st.tabs(tab_titles)
+def main() -> None:
+    """Main function to run the UI agent."""
+    # Parsing arguments for the MQTT broker address and port
+    parser = argparse.ArgumentParser(description="MQTT UI agent")
+    parser.add_argument(
+        "--broker", "-b",
+        type=str,
+        required=True,
+        help="MQTT broker address (e.g., 192.168.1.10)"
+    )
+    parser.add_argument(
+        "--port", "-p",
+        type=int,
+        default=1883,
+        help="MQTT broker port (default: 1883)"
+    )
+    args = parser.parse_args()
 
-# Populate each tab with proper helper functions
-with tabs[0]:
-    chatbot_ui()
+    # General control variables in the UI
+    if not "ui_initialized" in st.session_state:
+        # Connect the client to the MQTT broker
+        st.session_state.mqtt_client = mqtt.Client()
+        st.session_state.mqtt_client.connect(args.broker, args.port)
+        # Set the user ID for this session
+        st.session_state.user_id = 1
+        # Load the agents
+        with open("agents/config/agents_config.yaml", "r") as f:
+            config = yaml.safe_load(f)
+            st.session_state.agents = config["agents"]
+        # Add the docker control commands
+        for agent in st.session_state.agents.values():
+            agent["docker_run_command"] = f"docker run --rm -d --network host --name {agent['name']} {agent['name']} --broker {args.broker} --port {args.port} --user_id {st.session_state.user_id}"
+            agent["docker_stop_command"] = f"docker stop {agent['name']}"
+            agent["docker_remove_command"] = f"docker rm {agent['name']}"
+        # Load the workflows
+        with open("agents/config/workflows.yaml", "r") as f:
+            config = yaml.safe_load(f)
+            st.session_state.workflows = config["workflows"]
+        # Tab control state variables
+        st.session_state.active_tab = None
+        st.session_state.active_process = None
+        # Set the window as initialized
+        st.session_state.ui_initialized = True
 
-with tabs[1]:
-    st.header("Redes Neurais por planilha")
+    # Set the page configuration
+    st.set_page_config(page_title="Assistente Inteligente SAE", layout="wide")
+    st.title("Assistente Inteligente SAE")
 
-with tabs[2]:
-    st.header("Análise de PDFs")
+    # Create the tabs
+    st.session_state.tab_titles = [
+        "Chatbot", "Redes Neurais por planilha", "Análise de PDFs"]
+    tabs = st.tabs(st.session_state.tab_titles)
+    st.session_state.active_tab = st.session_state.tab_titles[0]
+
+    # Populate each tab with proper helper functions
+    with tabs[0]:
+        if st.session_state.active_tab != st.session_state.tab_titles[0]:
+            st.session_state.active_tab = st.session_state.tab_titles[0]
+        chatbot_ui()
+    print(st.session_state.active_tab)
+    with tabs[1]:
+        if st.session_state.active_tab != st.session_state.tab_titles[1]:
+            st.session_state.active_tab = st.session_state.tab_titles[1]
+        st.header("Redes Neurais por planilha")
+    print(st.session_state.active_tab)
+    with tabs[2]:
+        if st.session_state.active_tab != st.session_state.tab_titles[2]:
+            st.session_state.active_tab = st.session_state.tab_titles[2]
+        st.header("Análise de PDFs")
+    print(st.session_state.active_tab)
+
+
+if __name__ == "__main__":
+    main()
--- orchestrator/helper_chatbot.py
+++ orchestrator/helper_chatbot.py
@@ -1,31 +1,71 @@
-# helper_chatbot.py
-
+import paho.mqtt.client as mqtt
+from typing import Any
 import streamlit as st
 import os
 import sys
+import subprocess
 
 # Add the parent directory folder to find our modules
 root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(root_path)
 
-from ai_apis.chat_with_history import ChatBot
 os.environ["OLLAMA_ACCELERATE"] = "gpu"
 
+
+def assistant_response_callback(client: mqtt.Client, userdata: Any, msg: mqtt.MQTTMessage) -> None:
+    """Callback function for when a message is received on the subscribed topic.
+
+    Args:
+        client (mqtt.Client): The MQTT client instance.
+        userdata (Any): User-defined data of any type.
+        msg (mqtt.MQTTMessage): The received MQTT message.
+    """
+    # Decode the message payload
+    try:
+        data = msg.payload.decode('utf-8')
+    except UnicodeDecodeError:
+        print("Received message is not valid UTF-8.")
+        return
+    # Store the assistant response in the session state
+    if "assistant_response" not in st.session_state:
+        st.session_state.assistant_response = ""
+    st.session_state.assistant_response = data
+    st.new_chatbot_message = True
+
+
 def chatbot_ui():
+    """Streamlit UI for the chatbot agent."""
+    # Initialize the control variables in the UI
+    if "chatbot_ui_initialized" not in st.session_state:
+        # Initialize the proper agents from the workflow
+        workflow = st.session_state.workflows["chatbot"]
+        necessary_agents = workflow["agents"]
+        input_topics = workflow["input_topics"]
+        output_topics = workflow["output_topics"]
+        # Call each agent docker to start
+        for agent_name, input_topic, output_topic in zip(necessary_agents, input_topics, output_topics):
+            docker_run_command = st.session_state.agents[agent_name]["docker_run_command"]
+            docker_run_command += f" --input_topic {input_topic} --output_topic {output_topic}"
+            subprocess.Popen(docker_run_command, shell=True)
+        # Subscribe to the chatbot output topic
+        st.chatbot_input_topic = input_topics[0]
+        st.chatbot_output_topic = output_topics[0]
+        st.session_state.mqtt_client.subscribe(
+            st.chatbot_output_topic, qos=1)
+        # Create the chatbot callback, controlling message flow with a flag
+        st.session_state.mqtt_client.on_message = assistant_response_callback
+        st.new_chatbot_message = False
+        st.session_state.chatbot_ui_initialized = True
+
     # Display the app title and description
     st.title("Assistente de IA do GRIn")
 
-    # Initialize the chatbot
-    with st.spinner("Carregando o chatbot ..."):
-        model_id = "deepseek-r1:70b"
-        chatbot = ChatBot(model_id=model_id)
-
     # Initialize chat history
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+    if "chatbot_messages" not in st.session_state:
+        st.session_state.chatbot_messages = []
 
     # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
+    for message in st.session_state.chatbot_messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
@@ -33,13 +73,25 @@ def chatbot_ui():
     if prompt := st.chat_input("Digite sua mensagem."):
         with st.chat_message("user"):
             st.markdown(prompt)
-        assistant_response = ""
+        # Publish the user input to the MQTT broker
+        st.session_state.mqtt_client.publish(
+            st.session_state.chatbot_input_topic,
+            payload=prompt,
+            qos=1,
+            retain=False
+        )
+        # Run a spinner while waiting for the assistant response
+        with st.spinner("Aguardando resposta do assistente..."):
+            # Wait for the assistant response
+            while not st.new_chatbot_message:
+                st.session_state.mqtt_client.loop(timeout=1.0)
+            st.new_chatbot_message = False
+        # Get the assistant response from the MQTT broker
+        if "assistant_response" not in st.session_state:
+            st.session_state.assistant_response = ""
         response_placeholder = st.empty()
-        with st.chat_message("ai"):
-            for chunk in chatbot.chat(user_input=prompt):
-                assistant_response += chunk
-                response_placeholder.markdown(assistant_response)
-            chatbot.updateHistory(user_input=prompt, assistant_response=assistant_response)
-
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        st.session_state.messages.append({"role": "assistant", "content": assistant_response})
+        response_placeholder.markdown(st.session_state.assistant_response)
+        # Store the assistant response in the session state
+        st.session_state.chatbot_messages.append({"role": "user", "content": prompt})
+        st.session_state.chatbot_messages.append(
+            {"role": "assistant", "content": st.session_state.assistant_response})

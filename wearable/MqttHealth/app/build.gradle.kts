4d7c8bd4809ccfabc5d84e76c33d02dcfec494d0
Grin/viki <iago.biundini@gmail.com>
2025-10-01 14:20:54 -0300

    Adição do arquivo de chave JKS e atualização da configuração do projeto para Java 21. Modificações no .gitignore para ignorar a pasta .idea e atualização do versionCode e versionName no build.gradle.kts.

--- wearable/MqttHealth/app/build.gradle.kts
+++ wearable/MqttHealth/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.sae5g.mqttwearable"
         minSdk = 30
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 4
+        versionName = "0.0.4"
 
     }
 

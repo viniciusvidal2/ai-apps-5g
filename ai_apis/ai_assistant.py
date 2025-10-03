7e16abf823fb73429f7a8c293c1fff7b45ef6da6
viniciusvidal2 <viniciusvidal2@gmail.com>
2025-10-03 11:04:08 -0300

    removed deprecated versions

--- ai_apis/ai_assistant.py
+++ ai_apis/ai_assistant.py
@@ -1,6 +1,6 @@
-from langchain_community.embeddings import OllamaEmbeddings
-from langchain_community.vectorstores import Chroma
+from langchain_ollama import OllamaEmbeddings
 from langchain_ollama import ChatOllama
+from langchain_chroma import Chroma
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.prompts import ChatPromptTemplate, PromptTemplate

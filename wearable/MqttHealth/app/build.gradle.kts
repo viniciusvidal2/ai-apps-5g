383f609305737213cd48b4e0febbe317ecd883c3
DESKTOP-O42CJFS\iagob <iago.biundini@gmail.com>
2025-10-22 19:59:41 -0300

    Add new JKS files, public key, and related resources for signature management; updated build.gradle.kts with new version code and name.

--- wearable/MqttHealth/app/build.gradle.kts
+++ wearable/MqttHealth/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.sae5g.mqttwearable"
         minSdk = 30
         targetSdk = 34
-        versionCode = 4
-        versionName = "0.0.4"
+        versionCode = 11
+        versionName = "0.0.11"
 
     }
 

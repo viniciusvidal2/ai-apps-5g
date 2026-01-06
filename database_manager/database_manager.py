4d618aed62884e20d1893a4bb5ff93cf297b3059
viniciusvidal2 <viniciusvidal2@gmail.com>
2026-01-06 11:07:54 -0300

    improving database metadata and adding more documents fortesting

--- database_manager/database_manager.py
+++ database_manager/database_manager.py
@@ -36,13 +36,23 @@ class DatabaseManager():
         # Convert PDF to Docling's internal structured format
         result = self.converter.convert(document_path)
         chunks_iterator = self.chunker.chunk(dl_doc=result.document)
-        # Extract just the text from each chunk object
-        final_texts = [chunk.text for chunk in chunks_iterator]
-        # Metadatas and IDs
+        # Prepare data for insertion
+        final_texts = []
+        metadatas = []
+        ids = []
         document_name = self._get_document_name(document_path)
-        metadatas = [{"document_name": document_name} for _ in final_texts]
-        ids = [document_name + "_chunk_" +
-               str(i) for i in range(len(final_texts))]
+        for i, chunk in enumerate(chunks_iterator):
+            final_texts.append(chunk.text)
+            page_numbers = sorted(list(set(
+                prov.page_no for item in chunk.meta.doc_items for prov in item.prov if hasattr(prov, "page_no")
+            )))
+            # Create metadata and id for this specific chunk
+            metadatas.append({
+                "document_name": document_name,
+                "page_number": str(page_numbers)  # Chroma prefers strings or simple types
+            })
+            ids.append(f"{document_name}_chunk_{i}")
+        # Add to collection
         collection.add(
             documents=final_texts,
             metadatas=metadatas,
@@ -83,14 +93,21 @@ class DatabaseManager():
 if __name__ == "__main__":
     db_manager = DatabaseManager(
         db_path="./database_manager/chroma_db_testing")
-    pdf_paths = ["/home/vini/Downloads/Hotel SP Setembro 2025.pdf"]
+    pdf_paths = [
+        "/home/vini/Downloads/5g_docs/COE_ELET - 00 - CÓDIGO DE CONDUTA ELETROBRAS 2024 - COMPLIANCE.pdf",
+        "/home/vini/Downloads/5g_docs/PGC-GA-0001 - 02 - TRANSPORTE DE PASSAGEIROS E UTILIZAÇÃO DE VEÍCULOS - ADM.pdf",
+        "/home/vini/Downloads/5g_docs/PGC-GF-0004 - 03 - REEMBOLSO DE DESPESAS E VIAGENS - FI.pdf",
+        "/home/vini/Downloads/5g_docs/PGC-GSC-0001 - 01 - PGC-GSC-0001 - Procedimento de Avaliação de Fornecedores Rev Final - CONT.pdf",
+        "/home/vini/Downloads/5g_docs/PLT-0001 - 02 - PLT-0001 - 02 - POLÍTICA DE TECNOLOGIA DE INFORMAÇÃO TI.pdf",
+        "/home/vini/Downloads/5g_docs/PLT-0008 - 01 - PLT-0008- POLÍTICA DO SISTEMA DE GESTÃO INTEGRADA - GMASST.pdf",
+    ]
     for pdf_path in pdf_paths:
         db_manager.add_document(
             collection_name="my_collection", document_path=pdf_path)
     db_manager.inspect_collection(collection_name="my_collection")
 
     # Sample query
-    query = "Qual o local do hotel?"
+    query = "Quais são os compromissos da Santo Antônio Energia em relação à saúde, segurança e meio ambiente?"
     results = db_manager.query_collection(
         collection_name="my_collection", query_text=query, n_results=3)
     for doc, score in zip(results['documents'][0], results['distances'][0]):
